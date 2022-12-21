c6d79f93717bd02caafedb45c1c376120c08f9eb
Efrain Lemus <efraintlemusm@gmail.com>
2022-12-21 10:04:58 -0500

    edit header and mobile optimize

--- themes/insideoutcreative/header.php
+++ themes/insideoutcreative/header.php
@@ -25,31 +25,39 @@ endif;
 
 
 echo '<div class="container z-3 position-relative text-white">';
-echo '<div class="row align-items-center justify-content-end">';
-
-// echo '<div class="col-6 desktop-hidden">';
-// echo '<a id="navToggle" class="nav-toggle">';
-// echo '<div>';
-// echo '<div class="line-1 bg-white"></div>';
-// echo '<div class="line-2 bg-white"></div>';
-// echo '<div class="line-3 bg-white"></div>';
-// echo '</div>';
-// echo '</a>';
-// echo '</div>';
+echo '<div class="row align-items-center justify-content-between pt-2 pb-2">';
 
 
-echo '<div class="col-lg-6 text-center d-flex align-items-center justify-content-center">';
+$logo = get_field('logo','options');
 
-// echo '<div class="mobile-hidden">';
+echo '<div class="col-lg-2 col-md-4 col-6">';
+    echo '<a href="' . home_url() . '">';
+    echo wp_get_attachment_image($logo['id'],'full',"",['class'=>'w-100 h-auto pl-md-0 pl-4 pr-md-0 pr-4','style'=>'max-width:100%;']);
+    echo '</a>';
+    echo '</div>';
+
+    echo '<div class="col-md-4 col-6 desktop-hidden">';
+echo '<a id="navToggle" class="nav-toggle">';
+echo '<div>';
+echo '<div class="line-1 bg-white"></div>';
+echo '<div class="line-2 bg-white"></div>';
+echo '<div class="line-3 bg-white"></div>';
+echo '</div>';
+echo '</a>';
+echo '</div>';
+
+echo '<div class="col-lg-6 mobile-hidden text-center">';
+
+echo '<div class="d-flex align-items-center justify-content-center">';
 wp_nav_menu(array(
     'menu' => 'primary',
     'menu_class'=>'menu d-flex flex-wrap list-unstyled justify-content-center mb-0'
 ));
-// echo '</div>';
+echo '</div>';
 
 echo '</div>';
 
-echo '<div class="col-lg-3 text-right">';
+echo '<div class="col-lg-3 col-md-4 text-md-right text-center">';
 echo '<a href="tel:+1' . get_field('phone','options') . '" class="h2 text-white">' . get_field('phone','options') . '</a>';
 echo '</div>';
 
@@ -61,7 +69,7 @@ echo '</div>';
 
 echo '<div class="blank-space"></div>';
 
-echo '<header class="position-relative w-100" style="top:0;left:0;">';
+echo '<header class="position-relative w-100" style="top:0;left:0;min-height:80vh;">';
 
 $globalPlaceholderImg = get_field('global_placeholder_image','options');
 
@@ -69,7 +77,7 @@ $globalPlaceholderImg = get_field('global_placeholder_image','options');
 if(is_page()){
 if(has_post_thumbnail()){
 
-echo '<div class="position-absolute w-100 h-100 bg-attachment" style="background:url(' . get_the_post_thumbnail_url() . ');background-size:cover;background-attachment:fixed;top:0;left:0;"></div>';
+echo '<div class="position-absolute w-100 h-100 bg-attachment" style="background:url(' . get_the_post_thumbnail_url() . ');background-size:cover;background-attachment:fixed;background-position:center top;top:0;left:0;"></div>';
 
 } else {
 echo wp_get_attachment_image($globalPlaceholderImg['id'],'full','',['class'=>'w-100 h-100 bg-img position-absolute','style'=>'object-position:top;']);
@@ -78,18 +86,16 @@ echo wp_get_attachment_image($globalPlaceholderImg['id'],'full','',['class'=>'w-
 echo wp_get_attachment_image($globalPlaceholderImg['id'],'full','',['class'=>'w-100 h-100 bg-img position-absolute','style'=>'object-position:top;']);
 }
 
-$logo = get_field('logo','options');
-
 if(is_front_page()):
 echo '<section class="hero position-relative z-2">';
 
-if($logo){
-    echo '<div class="text-center pt-5">';
-    echo '<a href="' . home_url() . '">';
-    echo wp_get_attachment_image($logo['id'],'full',"",['class'=>'w-100 h-auto pl-md-0 pl-4 pr-md-0 pr-4','style'=>'max-width:500px;']);
-    echo '</a>';
-    echo '</div>';
-}
+// if($logo){
+//     echo '<div class="text-center pt-5">';
+//     echo '<a href="' . home_url() . '">';
+//     echo wp_get_attachment_image($logo['id'],'full',"",['class'=>'w-100 h-auto pl-md-0 pl-4 pr-md-0 pr-4','style'=>'max-width:500px;']);
+//     echo '</a>';
+//     echo '</div>';
+// }
 
 if(!is_page(55)) {
 echo '<div class="hero-content-inner text-white" style="padding:100px 0 350px;">';
