d9409344918718432907dc5109c2b397eda1e6f5
Efrain Lemus <efraintlemusm@gmail.com>
2023-01-24 11:58:25 -0700

    update single php

--- themes/insideoutcreative/single.php
+++ themes/insideoutcreative/single.php
@@ -4,16 +4,17 @@
     <div class="row justify-content-center">
     <?php get_template_part('partials/sidebar'); ?>
         <div class="col-lg-9 col-md-12 order-lg-2 order-1">
-        <p class="d-flex flex-wrap align-items-center mb-5">
-        <?php echo get_avatar( get_the_author_email(), '60', $default, $alt, array( 'class' => array( 'mr-2', 'img-avatar' ) ) ); ?>
-    <span class="mr-1"><?php echo get_the_author_posts_link(); ?></span> | <?php the_time('F jS, Y'); ?></p>
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+        <p class="d-flex flex-wrap align-items-center">
+        <?php the_time('F jS, Y'); ?></p>
+        <?php 
+        echo '<h1 class="aspira-bold pb-4">' . get_the_title() . '</h1>';
+
+        if ( have_posts() ) : while ( have_posts() ) : the_post();
 the_content();
 endwhile; else: ?>
 <p>Sorry, no posts matched your criteria.</p>
 <?php endif; ?>
-<hr>
-<?php comments_template(); ?>
+
         </div>
         <!-- <div class="col-lg-3"> -->
             
